9cb89b08b896a25e01074ffee16b91c7909d6437
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2016-07-17 19:43:43 +0200

    Corrected values restrictions for usage

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-	validates :usage, :inclusion => { :in => ["Win","Loose"]}
+	validates :usage, :inclusion => { :in => ["Win","Lose"]}
 	
 	#06.06.2016: overrides the as_json method to only display the id, usage and body in the API
 	def as_json(options={})
