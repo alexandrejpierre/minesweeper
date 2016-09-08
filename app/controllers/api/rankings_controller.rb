8bc695c0d253e1e389bcfdf4db6cad7ce9a9ccef
Alexandre PIERRE <alexandrej.pierre@icloud.com>
2016-09-08 21:50:11 +0200

    Update the rankings api

--- app/controllers/api/rankings_controller.rb
+++ app/controllers/api/rankings_controller.rb
@@ -5,20 +5,19 @@ module Api
 		respond_to :json
 		
 		def index
-			#~ #List of scores corresponding to the level required
-			#~ @table=Leaderboard.where("level = ?",ranking_params[:level])
-			#~ #List of the ten best scores on the level
-			#~ @a=@table.order(:score).first(10)
-			#~ #Best score of the user on the level
-			#~ @b=@table.where("name = ?",ranking_params[:name]).order(:score).first
-			#~ #Concatenation
-			#~ @a.append(@b)
-			#~ #Addition of the rank
-			#~ @a.each do |x|
-				#~ x.assign_attributes(rank: @table.where("score < ?",x.score).count + 1)
-			#~ end
-			#~ respond_with @a
-			respond_with Leaderboard.first
+			#List of scores corresponding to the level required
+			@table=Leaderboard.where("level = ?",ranking_params[:level])
+			#List of the ten best scores on the level
+			@a=@table.order(:score).first(10)
+			#Best score of the user on the level
+			@b=@table.where("name = ?",ranking_params[:name]).order(:score).first
+			#Concatenation
+			@a.append(@b)
+			#Addition of the rank
+			@a.each do |x|
+				x.assign_attributes(rank: @table.where("score < ?",x.score).count + 1)
+			end
+			respond_with @a
 		end
 	
 		
